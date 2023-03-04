133bd4625e26e04bcb3ba984e2a3e4387af6487d
wahyunanandika <wahyunanandika@gmail.com>
2023-03-04 12:49:24 +0700

    feat : share button for sharing pdf, only at client site, using vue-socials package, no need to handle at backend

---
+++ creativitae-client-user/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import LogoLinkedin from '../components/LoginLinkedin.vue'
+</script>
+
+<template>
+  <main>
+    <LogoLinkedin />
+    <TheWelcome />
+  </main>
+</template>
